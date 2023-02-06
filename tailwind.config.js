cbd6b85b7e0c428101d63fde94f2e730db1f9e56
AlvaJufinto <yeahbruh199@gmail.com>
2023-02-06 09:13:13 +0700

    Setting up folders and files

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      'white': '#FFFFFF',
+      'black': '#000000',
+      'blue': '#312E81',
+      'darkBlue': '#1D1C4D',
+      'orange': '#F97316',
+      'grey': '#768293',
+    },
+    extend: {},
+    screens: {
+      'sm': { 'max': '768px' },
+      'xl': { 'max': '1280px' },
+    },
+  },
+  plugins: [],
+}
